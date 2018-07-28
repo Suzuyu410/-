e9ba8c166fac15989b69daa417b580af8cdb56ec
Suzuyu410 <akurai4107@gmail.com>
2018-07-28 22:40:12 +0900

    初コミット

---
+++ Assets/Particle/Cloud/CloudController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudController : MonoBehaviour {
+    [SerializeField]
+    private float speed;
+   
+    [SerializeField]
+    private float yPosition;
+    float x;
+    float y;
+    float z;
+
+    // Use this for initialization
+    void Start()
+    {
+        x = -2.0f;
+        z = Random.Range(-1.0f, 1.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x += Time.deltaTime * speed;
+        y = yPosition;
+
+        if(x >= 2.0f)
+        {
+            x = -2.0f;
+        }
+
+        transform.position = new Vector3(x, y, z);
+    }
+}
